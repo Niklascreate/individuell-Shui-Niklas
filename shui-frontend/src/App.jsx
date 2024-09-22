e91cacd2e1f590966bf46c1ae56aaf5e719c9fcf
Adam Rehal <Niklasn89@gmail.com>
2024-09-22 14:56:14 +0200

    bugfix-1

--- shui-frontend/src/App.jsx
+++ shui-frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Flow from '../../shui-frontend/src/pages/flow/Flow.jsx'
 import Writemsg from './pages/writemsg/Writemsg.jsx';
 import "./App.css";
+import Header from './components/header/Header.jsx';
+
 
 function App() {
   return (
     <Router>
+      <Header />
       <div className='app'>
       <Routes>
         <Route path="/" element={<Flow />} />
--- shui-frontend/src/components/header/Header.jsx
+++ shui-frontend/src/components/header/Header.jsx
@@ -1,10 +1,13 @@
 import './header.css'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
-        <h1>Klagomuren</h1>
+    <Link to='/Flow'>
+    <header className='header-container'>
+        <h1 className='header-title'><span>K</span><span>l</span><span>a</span><span>g</span><span>o</span><span>m</span><span>u</span><span>r</span><span>e</span><span>n</span></h1>
     </header>
+    </Link>
   )
 }
 
