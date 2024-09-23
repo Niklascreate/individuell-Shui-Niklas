578b5a97dfcbf38742e2e65fe393884e96e9099e
Adam Rehal <Niklasn89@gmail.com>
2024-09-23 08:49:14 +0200

    deletefunktionen klar

--- shui-frontend/src/pages/flow/Flow.jsx
+++ shui-frontend/src/pages/flow/Flow.jsx
@@ -20,10 +20,29 @@ function Flow() {
     fetchInlagg();
   }, []); // Tom beroendelista innebär att effekten körs en gång vid montering
 
-const handleDelete = (id) => {
-  console.log('Tog bort ett inlägg:', id)
-  axios.delete
-}
+
+  const handleDelete = (id) => {
+    axios.delete(`https://g7jux13pha.execute-api.eu-north-1.amazonaws.com/post/${id}`)
+      .then(response => {
+        if(response.status === 200) { // Kontrollerar om raderingen lyckades
+          const updatedInlagg = inlagg.filter(post => post.id !== id);
+          setInlagg(updatedInlagg); // Uppdaterar inlägg till att inte inkludera det raderade inlägget
+        }
+      })
+      .catch(error => console.error('Något gick fel vid radering:', error));
+  }
+
+  const handlePost = (id) => {
+    axios.put(`https://g7jux13pha.execute-api.eu-north-1.amazonaws.com/post/${id}`)
+      .then(response => {
+        if(response.status === 200) {
+          const updatedInlagg = inlagg.filter(post => post.id !== id);
+          setInlagg(updatedInlagg);
+        }
+      })
+      .catch(error => console.error('Något gick fel vid uppdatering av meddelandet.', error));
+  }
+
 
   return (
     <div className='container-wrapper'>
@@ -32,7 +51,7 @@ const handleDelete = (id) => {
       ) : (
         inlagg.map((post) => (
           <section className='box-one' key={post.id}>
-            <article className='box-update'><ChangeBtn /> <span className='close'>X</span> </article>
+            <article className='box-update'><ChangeBtn /> <span onClick={() => handleDelete(post.id)} className='close'>X</span> </article>
             <p className="time">{post.tid}</p>
             <p className="content">{post.inlagg}</p>
             <p className="alias">{post.alias}</p>
